7a668ea9def083f7b3411ae24c4b831cd3bd6eea
Elston Misquitta <elston007@gmail.com>
2022-10-23 21:58:50 +0530

    Changes

---
+++ BlazorDemoUI/DataAccessLibrary/SqlDataAccess.cs
@@ -0,0 +1,50 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        public string ConnectionStringName { get; set; } = "Default";
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // Load Data
+        // [1] async Task<List<T>>  = Here mentioned the return type for the Task  <List<T>>
+        // [2] LoadData <T, U> (string sql,U parameters) = LoadData Method we have to mention the generic used also the parameters if need
+
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var data = await connection.QueryAsync<T>(sql, parameters);
+                return data.ToList();
+            }
+        }
+
+        // Save Data
+        public async Task SaveData<T>(string sql, T parameters)
+        {
+            string connectionString = _config.GetConnectionString(ConnectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.ExecuteAsync(sql, parameters);
+            }
+        }
+
+
+    }
+}
